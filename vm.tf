da6a060c5718afe52cb251ba2d6114546625fa66
Dan <d.lupas88@gmail.com>
2023-07-03 12:48:41 +0300

    first commit

---
+++ vm.tf
@@ -0,0 +1,35 @@
+resource   "azurerm_network_interface"   "myvm1nic"   { 
+   name   =   "myvm1-nic" 
+   location   =   "westus" 
+   resource_group_name   =   azurerm_resource_group.rg.name 
+
+   ip_configuration   { 
+     name   =   "ipconfig1" 
+     subnet_id   =   azurerm_subnet.frontendsubnet.id 
+     private_ip_address_allocation   =   "Dynamic" 
+     public_ip_address_id   =   azurerm_public_ip.myvm1publicip.id 
+   } 
+ } 
+
+
+ resource   "azurerm_windows_virtual_machine"   "example"   { 
+   name                    =   "myvm1"   
+   location                =   "West US" 
+   resource_group_name     =   azurerm_resource_group.rg.name 
+   network_interface_ids   =   [ azurerm_network_interface.myvm1nic.id ] 
+   size                    =   "Standard_B1s" 
+   admin_username          =   "adminuser" 
+   admin_password          =   "Password123!" 
+
+   source_image_reference   { 
+     publisher   =   "MicrosoftWindowsServer" 
+     offer       =   "WindowsServer" 
+     sku         =   "2019-Datacenter" 
+     version     =   "latest" 
+   } 
+
+   os_disk   { 
+     caching             =   "ReadWrite" 
+     storage_account_type   =   "Standard_LRS" 
+   } 
+ } 
