027797f011c59429dd0dc01aa462c854d430afcf
Dan <d.lupas88@gmail.com>
2023-07-03 13:58:27 +0300

    delete new folder

--- New folder/publicIP.tf
+++
@@ -1,7 +0,0 @@
-resource   "azurerm_public_ip"   "myvm1publicip"   { 
-   name   =   "pip1" 
-   location   =   "West US" 
-   resource_group_name   =   azurerm_resource_group.rg.name 
-   allocation_method   =   "Dynamic" 
-   sku   =   "Basic" 
- } 
