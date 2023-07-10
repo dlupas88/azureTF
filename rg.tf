807f73f71bfdcee1ea665218263eff5f46d683e2
Dan <d.lupas88@gmail.com>
2023-07-10 09:47:38 +0300

    fmt

--- rg.tf
+++ rg.tf
@@ -1,7 +1,7 @@
-resource   "azurerm_resource_group"   "rg"   { 
-   name   =   "1-4e2f3f83-playground-sandbox" 
-   location   =   "West US" 
- }
+resource "azurerm_resource_group" "rg" {
+  name     = "1-4e2f3f83-playground-sandbox"
+  location = "West US"
+}
 
 #terraform import "azurerm_resource_group.rg" "/subscriptions/0cfe2870-d256-4119-b0a3-16293ac11bdc/resourceGroups/1-4e2f3f83-playground-sandbox"
 
